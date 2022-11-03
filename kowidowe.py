fa7741866d07c7595f5c83924ad352c1f7a1f2f5
ninj4i <piotr.ptaszynski@outlook.com>
2022-11-04 00:07:36 +0100

    problemy problemy, ale jutro już piątek :D

--- kowidowe.py
+++ kowidowe.py
@@ -4,7 +4,7 @@ import pathlib
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import datetime
 
 
 """
@@ -29,20 +29,40 @@ for i in lista_plikow[:5]:
 arch_data = pd.read_csv(pathlib.Path('dane/mat_arch.csv'), sep = ';',decimal=' ', encoding= 'windows-1250')
 
 arch_data['Data'] = pd.to_datetime(arch_data['Data'], format='%d.%m.%Y')
-arch_data['Nowe przypadki'] = arch_data['Nowe przypadki'].astype('int')
+arch_data['Nowe przypadki'] = arch_data[ 'Nowe przypadki'].astype('int')
 
 
 plt.rcParams['figure.dpi']     = 100
 plt.rcParams['figure.figsize'] = 15,5
-print(arch_data)
+
+kokokoko = []
+kokokoko.append(pd.DataFrame().assign(data = arch_data.loc[:,'Data'], liczba_przypadkow = arch_data.loc[:,'Nowe przypadki']))
+#for plik in lista_plikow[:2]:
+#    ko = pd.read_csv(plik)
+#    print(ko.columns = ['Data', 'columns']
+#    kokokoko.append(ko.iloc[0,1])
+
 print(arch_data.dtypes)
 
-kokokoko1= pd.read_csv(lista_plikow[100], sep = ';',decimal=' ', encoding= 'windows-1250')
 
-kokokoko = pd.DataFrame().assign(data = arch_data.loc[:,'Data'], liczba_przypadkow = arch_data.loc[:,'Nowe przypadki'])
-kokokoko2= pd.DataFrame().assign(data = kokokoko1.loc[:0,'stan_rekordu_na'], liczba_przypadkow = kokokoko1.loc[0:,'Nowe przypadki'])
-koko =pd.concat(kokokoko, kokokoko2)
+koko =pd.concat(kokokoko)
 
-sns.scatterplot(data = koko, x = 'data', y = 'liczba_przypadkow', size= 1, legend = False)
-plt.show()
+#sns.scatterplot(data = koko, x = 'data', y = 'liczba_przypadkow', size= 1, legend = False)
+#plt.show()
 print(kokokoko)
+
+
+x = []
+#x.append(arch_data)
+
+for i, _ in enumerate(lista_plikow):
+    nazwa_kolumny = 'liczba_przypadkow' if 'liczba_przypadkow' in list(pd.read_csv(lista_plikow[i], sep = ';',decimal=' ', encoding= 'windows-1250').columns.values) else 'liczba_wszystkich_zakazen'
+    print(pd.to_datetime(str(lista_plikow[i])[11:19],format='%Y%m%d')-datetime.timedelta(days = 1))
+    print(f"\t {pd.read_csv(lista_plikow[i], sep = ';',decimal=' ', encoding= 'windows-1250').loc[0,nazwa_kolumny]}")
+    x.append([pd.to_datetime(str(lista_plikow[i])[11:19],format='%Y%m%d')-datetime.timedelta(days = 1),\
+        pd.read_csv(lista_plikow[i], sep = ';',decimal=' ', encoding= 'windows-1250').loc[0,nazwa_kolumny]])
+
+
+print(pd.DataFrame(x))
+
+sns.scatterplot(data = pd.concat(x))
