600703beafc557a12888cf70ca74325717119fc5
ninj4i <piotr.ptaszynski@outlook.com>
2022-11-12 23:13:03 +0100

    q

--- kowidowe.py
+++ kowidowe.py
@@ -11,17 +11,18 @@ from cycler import cycler
 """
 wstępnie niepotrzebne linie bo mamy na dysku pliki
 """
-#r = requests.get('https://arcgis.com/sharing/rest/content/items/a8c562ead9c54e13a135b02e0d875ffb/data')
-#zawartosc = r.contentxs['A'].set_xticks(pd.to_datetime([f'{yr}-{mo}-01' for mo in range(1,13) for yr in range(2020, 2023)]))
-axs['A'].set_xticklabels(rotation = 45, size = 7, labels = [f'{yr}-{mo}' for mo in range(1,13) for yr in range(2020, 2023)])
-axs['A'].legend(fontsize =  10, loc = 2)
+r = requests.get('https://arcgis.com/sharing/rest/content/items/a8c562ead9c54e13a135b02e0d875ffb/data')
+zawartosc = r.content
+#['A'].set_xticks(pd.to_datetime([f'{yr}-{mo}-01' for mo in range(1,13) for yr in range(2020, 2023)]))
+#axs['A'].set_xticklabels(rotation = 45, size = 7, labels = [f'{yr}-{mo}' for mo in range(1,13) for yr in range(2020, 2023)])
+#axs['A'].legend(fontsize =  10, loc = 2)
 
-#with open('dane//dane.zip', 'wb') as plik:
-#    plik.write(zawartosc)
+with open('dane//dane.zip', 'wb') as plik:
+    plik.write(zawartosc)
 
 
-#with zipfile.ZipFile('dane//dane.zip') as spaklowany_plik:
-#    spaklowany_plik.extractall(pathlib.Path('dane/pliki'))
+with zipfile.ZipFile('dane//dane.zip') as spaklowany_plik:
+    spaklowany_plik.extractall(pathlib.Path('dane/pliki'))
 
 lista_plikow = list(pathlib.Path.glob(pathlib.Path('dane/pliki'), '*.csv'))
 
@@ -75,7 +76,7 @@ przypadki = pd.DataFrame(przypadki)
 
 #plt.rcParams['size']
 plt.rcParams['figure.dpi']     = 100
-plt.rcParams['figure.figsize'] = 15,12
+plt.rcParams['figure.figsize'] = 10,8
 plt.rcParams['axes.prop_cycle'] = cycler('color', ['cadetblue','orangered','gold', 'navy'])
 
 nazwa_kolumny = ['liczba_wszystkich_zakazen', 'liczba_nowych_zakazen', 'liczba_ponownych_zakazen']
@@ -106,7 +107,7 @@ for i, _ in enumerate(lista_plikow):
 zgony_woj1 = pd.concat(zgony_woj)
 
 woj_labels = odczytane_dane['wojewodztwo'].values
-zgony_woj  = zgony_woj1.astype('int16')
+zgony_woj  = zgony_woj1.astype('float16')
 
 fig, axs = plt.subplot_mosaic(mosaic="""
 AAAC
@@ -115,14 +116,13 @@ BBBD
 
 
 sns.lineplot(data = przypadki1, x = przypadki1['Data'], y = 'liczba_wszystkich_zakazen', ax = axs['A'], label = 'Wszystkie przypadki dzienne')
-sns.lineplot(data = przypadki1.rolling(window = 7).mean(), x = przypadki1['Data'], y = 'liczba_wszystkich_zakazen', ax = axs['A'], label = 'Wygładzony przebieg dziennych zakarzeń')
-sns.lineplot(data = przypadki1.rolling(window = 7).mean(), x = przypadki1['Data'], y = 'liczba_nowych_zakazen', ax = axs['A'], label = 'Wygładzony przebieg nowych dziennych zakarzeń')
-sns.lineplot(data = przypadki1.rolling(window = 7).mean(), x = przypadki1['Data'], y = 'liczba_ponownych_zakazen', ax = axs['A'], label = 'Wygładzony przebieg PONOWNYCH dziennych zakarzeń')
-#sns.lineplot(data = X, x = 'Data', y = X['Liczba Przypadkow'].cumsum(), size= 1, legend=False)//////////////////
+sns.lineplot(data = przypadki1.rolling(window = 7,min_periods = 1).mean(), x = przypadki1['Data'], y = 'liczba_wszystkich_zakazen', ax = axs['A'], label = 'Wygładzony przebieg dziennych zakarzeń')
+sns.lineplot(data = przypadki1.rolling(window = 7,min_periods = 1).mean(), x = przypadki1['Data'], y = 'liczba_nowych_zakazen', ax = axs['A'], label = 'Wygładzony przebieg nowych dziennych zakarzeń')
+sns.lineplot(data = przypadki1.rolling(window = 7,min_periods = 1).mean(), x = przypadki1['Data'], y = 'liczba_ponownych_zakazen', ax = axs['A'], label = 'Wygładzony przebieg PONOWNYCH dziennych zakarzeń')
 
-a
+
 axs['A'].set_ylabel('ilość dziennych zakarzeń')
-axs['A'].grid(axis = 'both', c = '0.90')
+axs['A'].grid(axis = 'both', c = '0.50')
 axs['A'].set_title('Dobowa liczba przypadków zakażeń', fontdict = {'fontsize' : 10, 'weight' : 'bold'}, loc = 'left')
 
 sns.lineplot(data = zgony1.rolling(window = 7).mean(), x = 'Data', y = 'zgony', legend = True, ax = axs['B'], label = 'Wszystkie zgony')
@@ -133,23 +133,27 @@ axs['B'].set_xticks(pd.to_datetime([f'{yr}-{mo}-01' for mo in range(1,13) for yr
 axs['B'].set_xticklabels(rotation = 45, size = 7, labels = [f'{yr}-{mo}' for mo in range(1,13) for yr in range(2020, 2023)])
 axs['B'].legend(fontsize =  10, loc = 2)
 axs['B'].set_ylabel('ilość dziennych zgonów')
-axs['B'].grid(axis = 'both', c = '0.90')
-axs['B'].set_title('Dobowa liczba zgonów', fontdict = {'fontsize' : 10, 'weight' : 'bold'}, loc = 'left', y = -0.15)
+axs['B'].grid(axis = 'both', c = '0.50')
+axs['B'].set_title('Dobowa liczba zgonów', fontdict = {'fontsize' : 10, 'weight' : 'bold'}, loc = 'left')
 
-#sns.barplot (data = przypadki_woj1, ax = axs['C'], orient= 'h')
+
 axs['C'].barh(y = [x for x in range(1,17)], width = przypadki_woj1.sum(), height = 0.9)
 axs['C'].set_yticks([x for x in range(1,17)])
 axs['C'].yaxis.tick_right()
 axs['C'].set_yticklabels(woj_labels[1:])
-axs['C'].grid(axis = 'x', c = '0.90' )
+axs['C'].set_xticks(ticks = [x*200000 for x in range(1, 5)])
+axs['C'].set_xticklabels(rotation = -45,size = 7, labels = [x*200000 for x in range(1,5)])
+axs['C'].grid(axis = 'x', c = '0.50' )
 axs['C'].set_title(f'liczba przypadków zakażeń\nz podziałem na województwa\nod dn {przypadki["Data"][0].strftime("%Y-%m-%d")}', fontdict = {'fontsize' : 10, 'weight' : 'bold'})
 
-axs['D'].barh(y = [x for x in range(1,17)], width = przypadki_woj1.sum(), height = 0.9)
+axs['D'].barh(y = [x for x in range(1,17)], width = zgony_woj.sum(), height = 0.9, color = 'tab:red')
 axs['D'].set_yticks([x for x in range(1,17)])
 axs['D'].yaxis.tick_right()
 axs['D'].set_yticklabels(woj_labels[1:])
-axs['D'].grid(axis = 'x', c = '0.90' )
-axs['D'].set_title(f'liczba przypadków zakażeń\nz podziałem na województwa\nod dn {przypadki["Data"][0].strftime("%Y-%m-%d")}', fontdict = {'fontsize' : 10, 'weight' : 'bold'})
+axs['D'].set_xticks(ticks = [x*2000 for x in range(1, 8)])
+axs['D'].set_xticklabels(rotation = -45,size = 7, labels = [x*2000 for x in range(1, 8)])
+axs['D'].grid(axis = 'x', c = '0.50' )
+axs['D'].set_title(f'liczba dziennych przypadków zakażeń\nz podziałem na województwa\nod dn {przypadki["Data"][0].strftime("%Y-%m-%d")}', fontdict = {'fontsize' : 10, 'weight' : 'bold'})
 
 
 
@@ -160,7 +164,15 @@ plt.tight_layout()
 
 plt.show()
 
-
-
+przypadki11 = przypadki1.rolling(7,min_periods = 1).mean()
+sns.lineplot(data = przypadki11, x = 'Data', y = 'liczba_wszystkich_zakazen', label = 'Wszystkie przypadki dzienne')
+plt.show()
+plt.tight_layout()
+print(len(przypadki1))
+print(len(przypadki1.rolling(7,min_periods = 1).mean()))
 
 #print(pd.read_csv(lista_plikow[1], sep = ';',decimal=' ', encoding= 'utf-8',index_col='teryt').iloc[1:,[1]].transpose())
+
+
+
+print(zgony_woj1.sum(axis = 1))
